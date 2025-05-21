7c34827dfb8f6541e39c3215757596ace0be9a15
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-05-21 13:15:28 +0200

    Add element visibility toogle

--- scripts/uiChangesScript.js
+++ scripts/uiChangesScript.js
@@ -2,3 +2,7 @@ function addContentToUI(data, uiElement) {
   const outputUI = document.querySelector(`${uiElement}`);
   outputUI.innerHTML = data;
 }
+function toggleElVisibility(uiElement) {
+  let element = document.getElementById(uiElement);
+  element.style.display = element.style.display === "block" ? "none" : "block";
+}
